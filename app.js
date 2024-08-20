2fec8eb178a019f5faa149b32e2f5b081c1c370b
Your Name <you@example.com>
2024-08-20 22:44:50 +0800

    feat: messages view wip

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
+const path = require("node:path");
 
 const indexRouter = require("./routes/indexRouter");
-const newMessagesRouter = require("./routes/newMessagesRouter");
 
 app.use("/", indexRouter);
-app.use("/new", newMessagesRouter);
+
+app.use(express.urlencoded({ extended: true }));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 const PORT = 3000;
 app.listen(PORT, () => {
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -15,8 +15,23 @@ const { Router } = require("express");
 
 const indexRouter = Router();
 
+indexRouter.get("/new", function (req, res) {
+	res.render("form", { title: "Mini Messageboard", messages: messages });
+	return res.end();
+});
+
+indexRouter.post("/new", function (req, res) {
+	console.log(req.body);
+	// console.log(JSON.stringify(req.body));
+	// req.body.added = new Date();
+	messages.push({ text: "test", added: new Date() });
+	res.redirect("/");
+	return res.end();
+});
+
 indexRouter.get("/", function (req, res) {
-	res.send("index");
+	res.render("index", { title: "Mini Messageboard", messages: messages });
+	return res.end();
 });
 
 module.exports = indexRouter;
